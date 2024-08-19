932cdcfdf2da3cf769a62d6a9ae15dc7f0886737
Terry Aney <terry.aney@icloud.com>
2024-08-19 10:12:08 -0500

    Initial commit.

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Topshelf;
+
+namespace TopshelfPreventingStop
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var service = HostFactory.Run(x =>
+            {
+                x.Service<Service>();
+
+                x.RunAsLocalSystem();
+
+                x.SetDescription("Service displaying conditional shutdown.");
+                x.SetDisplayName("KAT Conditional Shutdown");
+                x.SetServiceName("KATShutdown");
+
+                x.EnableShutdown();
+                x.SetStopTimeout(TimeSpan.FromSeconds(1));
+                x.StartAutomaticallyDelayed();
+            });
+
+            var exitCode = (int)Convert.ChangeType(service, service.GetTypeCode());  //11
+            Environment.ExitCode = exitCode;
+        }
+    }
+}
---
+++ Service.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Topshelf;
+
+namespace TopshelfPreventingStop
+{
+    internal class Service : ServiceControl
+    {
+        int shutdownRequests;
+
+        public bool Start(HostControl hostControl)
+        {
+            // Nothing to do here...
+            return true;
+        }
+
+        public bool Stop(HostControl hostControl)
+        {
+            shutdownRequests++;
+
+            return shutdownRequests >= 3;
+        }
+    }
+}
